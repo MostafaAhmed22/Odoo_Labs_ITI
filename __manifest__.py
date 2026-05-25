768670f441223fd1a66e9ef744ec6bdc0c3ca0a9
Mostafa Ahmed <modrsh2002@gmail.com>
2026-05-25 19:17:02 +0300

    lab 4:add security group and rules for user and manager

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,11 @@
     'depends': ['base', 'crm'],
 
     'data': [
+        'security/sec_groups.xml',
+        'security/rec_rules.xml',
         'security/ir.model.access.csv',
+        'reports/patient_report.xml',
+        'reports/patient_report_actions.xml',
         'views/patient_views.xml',
         'views/department_views.xml',
         'views/doctor_views.xml',
